e223076d8ec0c10fc98e04a5e7da071ee278e609
jmz <292648912@qq.com>
2024-07-10 21:18:56 +0800

    upload framework

---
+++ Assets/Framework/Audio/AudioManager/AudioButtonClickComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class AudioButtonClickComponent : MonoBehaviour
+{
+    public string audioName = "";
+
+    public float volume = 1f;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        if (ResourcesConfigManager.GetIsExitRes(audioName))
+        {
+            AudioPlayManager.PlaySFX2D(audioName, volume);
+        }
+        else
+        {
+            Debug.LogError("不存在音频文件：" + audioName);
+        }
+    }
+}
---
+++ Assets/Framework/Input/Events/IInputOperationEventBase.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 操作事件
+/// </summary>
+public abstract class IInputOperationEventBase : IInputEventBase
+{
+}
